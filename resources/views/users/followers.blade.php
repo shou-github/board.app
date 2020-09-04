b183699dec9f2627c4be92145e7294444ef7f429
shou <akishou0211@gmail.com>
2020-09-05 08:29:33 +0900

    add follow, unfollow

--- resources/views/users/followers.blade.php
+++ resources/views/users/followers.blade.php
@@ -9,12 +9,8 @@
         <div class="col-sm-8">
             {{-- タブ --}}
             @include('users.navtabs')
-            @if (Auth::id() == $user->id)
-                {{-- 投稿フォーム --}}
-                @include('boards.form')
-            @endif
-            {{-- 投稿一覧 --}}
-            @include('boards.boards')
+            {{-- ユーザ一覧 --}}
+            @include('users.users')
         </div>
     </div>
 @endsection
